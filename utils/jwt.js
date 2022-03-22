d8ca7912f3793631e450f5967b348920a0b3384b
shosetim <shosetim32@gmail.com>
2022-03-22 22:25:47 +0800

    httponly

--- utils/jwt.js
+++ utils/jwt.js
@@ -16,7 +16,7 @@ const attachCookieToResponse = ({ res, user }) => {
   const token = createJWT({ payload: user });
   const oneDay = 1000 * 60 * 60 * 24;
   res.cookie('token', token, {
-    httpOnly: true,
+    // httpOnly: true,
     expires: new Date(Date.now() + oneDay),
     // secure: process.env.NODE_ENV === 'production',
     signed: true,
