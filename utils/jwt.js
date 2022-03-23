5b68201bd25805a59d645aa31f1102633f7cf1e1
shosetim <shosetim32@gmail.com>
2022-03-23 21:29:54 +0800

    cookie

--- utils/jwt.js
+++ utils/jwt.js
@@ -20,7 +20,6 @@ const attachCookieToResponse = ({ res, user }) => {
     expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === 'production',
     signed: true,
-    domain: 'http://localhost:8080',
   })
 }
 
