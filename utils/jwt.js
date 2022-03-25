97d6be89e69a4ff9a7957962fc7e69d45fbc8a9b
shosetim <shosetim32@gmail.com>
2022-03-25 21:48:18 +0800

    cookie

--- utils/jwt.js
+++ utils/jwt.js
@@ -21,6 +21,7 @@ const attachCookieToResponse = ({ res, user }) => {
     signed: true,
     sameSite: 'none', // sameSite:'none'
     secure: false,
+    domain: 'http://192.168.0.11:8080',
   })
 }
 
